b0b9ee5fc7a8f51edec404cfb753e2f1f079b604
iModyHK <48744133+iModyHK@users.noreply.github.com>
2025-01-08 15:02:14 +0300

    Update config.lua
    Add a config for a hotkey to open the the vote

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 Config.VotingTime = 60 -- Voting time in seconds
 Config.CooldownTime = 3600 -- Cooldown time in seconds (60 minutes)
+Config.Hotkey = 168 -- Hotkey to open the vote menu -- https://docs.fivem.net/docs/game-references/controls/
 
 -- Chat Configuration
 Config.ChatUserName = 'SYSTEM'
